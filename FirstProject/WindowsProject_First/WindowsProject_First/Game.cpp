d2d1a41e5db4d30bc3ba4894aa31731af2b4f9eb
Chanhyeok <whd6428@gmail.com>
2024-10-10 20:38:19 +0900

    - 시간 관리자 추가

--- FirstProject/WindowsProject_First/WindowsProject_First/Game.cpp
+++ FirstProject/WindowsProject_First/WindowsProject_First/Game.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "Game.h"
+#include "InputManager.h"
+#include "TimeManager.h"
 
 Game::Game()
 {
@@ -11,10 +13,21 @@ Game::~Game()
 
 void Game::Init(HWND hwnd)
 {
+	_hwnd = hwnd;
+	_hdc = ::GetDC(hwnd);
+
+	::GetClientRect(hwnd, &_rect);
+
+	GET_SINGLE(TimeManager)->Init();
+	GET_SINGLE(InputManager)->Init(hwnd);
+
+	
 }
 
 void Game::Update()
 {
+	GET_SINGLE(InputManager)->Update();
+	GET_SINGLE(TimeManager)->Update();
 }
 
 void Game::Render()
